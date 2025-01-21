bdb4ca8a2cb9eded65361984e7631a713659cc73
Santosh Managuli <santoshmanaguli98@gmail.com>
2025-01-21 16:16:11 +0530

    added routing to home, search apge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { SeatBookingComponent } from './components/seat-booking/seat-booking.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'search',
+    component: SearchComponent,
+  },
+  {
+    path: 'seat-booking',
+    component: SeatBookingComponent,
+  },
+  {
+    path:"**",
+    component: NotFoundComponent
+  }
+];
